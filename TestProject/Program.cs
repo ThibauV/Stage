398eb88cdad1e8e09657a755cd569d9d97c9768b
ThibauVerberckmoes <Verberckmoesthibau@gmail.com>
2019-03-25 16:41:39 +0100

    ADD: TestProject

---
+++ TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
